9920b78053118cb081956e19c401c317aac16be5
Feick32 <hamitozer422@gmail.com>
2024-11-29 21:10:11 -0500

    Update tryout.js
    yo

--- tryout.js
+++ tryout.js
@@ -1,8 +1,92 @@
-let mami = 12;
+console.log('mami')
 
 
 
-while(mami >5){
-    console.log(mami)
-    mami--;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let newar = numbers.map(kare)
+function kare(num){
+    if(num % 2 == 0){
+        return Math.sqrt(num)
+    }
+    else{
+        return num;
+    }
 }
+
+console.log(newar) */
+
+
+
+
+
+
+
+
+
+
+
+
+/* Push x (dizinin sonuna yeni elemen ekler, eger yeni bir objeye atiyacaksanda onun uzunlugunu atar)
+Pop x (Amacı: Dizinin sonundaki elemanı kaldırır (çıkartır) ve kaldırılan elemanı döner. pop metodu doğrudan diziyi değiştirir, yani dizinin mevcut halini etkiler , Geri Dönüş Değeri: Diziden kaldırılan (çıkartılan) elemanı döner.)
+shift: Dizinin başından eleman kaldırır.
+unshift: Dizinin başına eleman ekler.
+
+
+Map
+filter 
+reduce 
+foreach 
+find 
+some 
+every 
+
+split(""):
+Kelimeyi bir karakter dizisine çevirir. Örneğin, "elma" → ["e", "l", "m", "a"].
+reverse():
+Diziyi ters çevirir. Örneğin, ["e", "l", "m", "a"] → ["a", "m", "l", "e"].
+join(""):
+Diziyi tekrar birleştirir. Örneğin, ["a", "m", "l", "e"] → "amle".
+map:
+Bu işlemi her bir eleman için uygular.
+
+Math.round, Math.random, Math.max, Math.min
+
+
+then, catch, finally
+
+async ve await kullanımı
+
+fetch, then, catch */
